17fd61266a39b486c45775acc089ca2c294f95d1
YumeMichi <do4suki@gmail.com>
2020-10-09 19:33:10 -0700

    joan: Enable double tap to wake feature

--- pa_joan.mk
+++ pa_joan.mk
@@ -40,7 +40,8 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model ro
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-pa
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
